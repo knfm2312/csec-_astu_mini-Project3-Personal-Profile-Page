548ef278381a93db73273032fede1993e68dd675
knfm2312 <yabsr23@gmail.com>
2025-12-19 20:44:29 +0300

    Update1

--- mini3/script.js
+++ mini3/script.js
@@ -5,4 +5,11 @@ let isOn =false;
 button.addEventListener('click',()=>{
     isOn=!isOn
     h1.textContent = isOn ? 'ON' : 'OFF';
+    if (isOn) {
+    h1.style.color = "black";
+    document.body.style.backgroundColor = "white"; 
+  } else {
+    h1.style.color = "black";
+    document.body.style.backgroundColor = "black"; 
+  }
 });
